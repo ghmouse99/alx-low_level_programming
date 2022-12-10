8ebc9bf3952c6122335fed08e086d1e8f0f21ab1
Eric insaidoo <berrickinsaidoo4@gmail.com>
2022-12-09 16:21:03 -0800

    Task 0

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ * main - print a randon number
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					if (n > 0)
+						printf("%d is positive\n . n");
+					else if (n < 0)
+						printf("%d is negative\n , n");
+					else
+						("%d is zero\n , n");
+
+					return (0);
+
+}
