7a31896fabdfd16e9ee33416e5ec41eee60cacb4
Eric insaidoo <berrickinsaidoo4@gmail.com>
2022-12-21 19:16:09 -0800

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 -  set pointer param to 98
+ *
+ * @n: int type pointer of 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is will be the readme file for the pointers task from alx
