055c661a96f9a197cc5710c20f159531e95d3f96
Eric insaidoo <berrickinsaidoo4@gmail.com>
2022-12-21 20:23:33 -0800

    updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - return the length of the string
+ * @s: char type pointer
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for(c = 0; s[c] != '\0'; c++);
+
+	return (c);
+}
